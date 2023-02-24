be9b4d1130800417f007d611048c69b8d5c5445f
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-02-24 19:14:46 +0100

    added endpoints to switch to admin, admin count list of daily sign up, admin get list of users, and admin middleware

--- routes/analytics.routes.js
+++ routes/analytics.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { getSignupsForToday } = require("../controllers/analytics.controller");
+const { authenticate, isAdmin} = require("../middleware/auth");
 
-router.get("/signups", getSignupsForToday);
+router.get("/signups", authenticate, isAdmin, getSignupsForToday);
 
 module.exports = router;
